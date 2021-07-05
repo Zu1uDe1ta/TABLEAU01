a2896edb617df7f210c7c112c1f209063df02ccd
Chris Chavez <chrischavezdataengineer@gmail.com>
2021-07-05 12:40:31 -0400

    review program for bugs

--- VS CODE TUTORIAL/Review_ValidateList.py
+++ VS CODE TUTORIAL/Review_ValidateList.py
@@ -4,44 +4,47 @@
 # 	Date: 06 JULY 2021
 # 	Class: CMPSC 132
 # 	Description: Implement Monte Carlo Method to Integrate a function.
+class Review_ValidateList:
+	def __init__(self):
+		self.sudoku_row = print(int(input("Please enter your Sudoku puzzle row for verification:  ")))
+
+	def sudoku_row_input(sudoku_row):
+			if ele in range (1, 10):
+				sudoku_row.append(ele)
+			else:
+				print("Your input is not acceptable value.")
+
+	def verify_sudoku_row_length(sudoku_row):
+		# chekc if input is a list
+		if not isinstance(sudoku_row, list): 
+			print ("Input was not a list")
+
+	def sudoku_row_length(sudoku_row):
+		# List should contain 9 elements 
+		if len(sudoku_row) < 9:
+			print ("This Sudoku row does not contain enough digits")
+		elif len(sudoku_row) > 9:
+			print ("This Sudoku row contains to many digits")
+		else: 
+			return sudoku_row
+
+	def value_repeated(): 
+
+		# Iterate each row
+		for ele in sudoku_row: 
+
+			# Define array to append found numbers
+			found = [] 
+
+			# Iterate each number in the row
+			for digit in row: 
+				# Check if current value is already foudn 
+				if digit not in found: 
+					# Store found values 
+					found.append(digit)
+				else: 
+					# Returns True if when it sees a repated number
+					print("This Sudoku row is not valid, it has repeated values.")
 
 
-
-sudoku_row = []
-int(input("Please enter your Sudoku puzzle row for verification:  "))
-
-for i in range(1, 10):
-	ele = int(input())
-
-	sudoku_row.append(ele)
-
-
-def verify_board(sudoku_row):
-
-	# chekc if input is a list
-	if not isinstance(sudoku, list): 
-		return False
-
-	# List should contain 9 elements 
-	if len(sudoku) != 9:
-		return ("List does not contain enough digits")
-
-	for row in sudoku:
-
-	# Check if rows are strings or lists
-		if not isinstance (row, (list, string)):
-			return False
-
-	# The length of both lists and sgtrings must equal 9
-	# Use the join method to compare both strings and lists 
-	text = ''.join(row)
-	if len(text) != 9:
-		return False
-
-	# Finally, check if row is digits-only
-	if not text.isdigit():
-		return False 
-
-# Else, return True
-	return True
-
+Review_ValidateList()
